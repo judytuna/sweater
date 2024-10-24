65f40d64db2018d3bdbb064252038944e734f026
Judy Tuan <315395+judytuna@users.noreply.github.com>
2024-10-24 13:57:54 -0700

    shove rhymed in

--- api/swapi.rb
+++ api/swapi.rb
@@ -79,12 +79,55 @@ require 'logger'
 # Avoids name conflicts when multiple functions are present in the /api folder
 Handler = Proc.new do |req, res|
   # Swapi::Handler # HEYDEV! Refer to your custom module
+
+  def rhyme_line(line)
+    words = line.split(/[\s\n]+/)
+    result = words.map do |word|
+      word.gsub(/[^\w\d]+/,'')
+    end.reject do |word|
+      word.strip.length.zero? || word =~ /[,'’]/
+    end.map do |word|
+      tagged = @tagger.add_tags(word)
+      xml = Nokogiri::XML(tagged)
+      part = xml.children.first.name
+      is_blacklisted = !/^[vnj]/.match(part)
+
+      if is_blacklisted
+        word
+      else
+
+        syllables_to_match = word.to_phrase.syllables
+        rhymes = word.to_phrase.flat_rhymes.select do |rhymed_word|
+          rhymed_word.to_phrase.syllables == syllables_to_match
+        end
+        rhymed = rhymes.sort_by do |rhyme|
+          @words.find_index(rhyme) || 0
+        end.select do |rhyme|
+          !/[\d]/.match(rhyme)
+        end.first(10).sample
+
+
+        rhymed || word
+      end
+    end.join(' ')
+  end
+
+
   res.status = 200
-  res['Content-Type'] = 'text/text; charset=utf-8'
+  res['Content-Type'] = 'application/json; charset=utf-8'
 	if req.body
-		text = req.body
-		res.body = "You submitted: #{text}!"
+    @words = File.read('frequencies').lines
+    @tagger = EngTagger.new
+    q = JSON.parse(req.body.read)['text']
+    rhymed = q.lines.map do |line|
+      rhyme_line(line)
+    end.to_a.join("\n")
+
+
+    # json :rhymed => rhymed
+    puts rhymed.json
+    res.body = rhymed.json
 	else
-		res.body = "No text data in query"
+		res.body = "No data in req.body"
 	end
 end
